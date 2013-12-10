f08d46d6af7623959dec4c32e4224b15ddd864fa
Petr Studeny <studenyp@gmail.com>
2013-12-10 13:57:44 +0100

    Added support for resizing

--- calibratescreen.py
+++ calibratescreen.py
@@ -8,24 +8,28 @@ version: 0.1
 import Tkinter as tk
 
 class mainFrame(object):
-    def __init__(self, root):
+    def __init__(self, root, title= "color bars"):
         self.f = tk.Frame(root, padx=5, pady=5)
+        root.title(title)
         tk.Grid.rowconfigure(root,0,weight=1)
         tk.Grid.columnconfigure(root,0,weight=1)
         self.f.grid(row=0,column=0,sticky='nesw')
-        self.c = tk.Canvas(self.f, bg="red", width=300, height=50)
-        self.c.grid(row=0, column=0, sticky='nesw')
-        tk.Grid.rowconfigure(self.f,0,weight=1)
-        tk.Grid.columnconfigure(self.f,0,weight=1)
+       # self.c = tk.Canvas(self.f, bg="red", width=300, height=50)
+       # self.c.grid(row=0, column=0, sticky='nesw')
+       # tk.Grid.rowconfigure(self.f,0,weight=1)
+       # tk.Grid.columnconfigure(self.f,0,weight=1)
 
 class colorBars(object):
-    def __init__(self, root, width, height, title= "color bars"):
-        self.w = tk.Canvas(root, width=width, height=height)
-        self.w.pack()
+    def __init__(self, root, width, height):
         self.width = width
         self.height = height
         self.bheight = 256/4
         self.bitwidth = 7
+        self.w = tk.Canvas(root, width=width, height=height, bg="black")
+        self.w.bind("<Configure>",self.redraw)
+        self.w.grid(row=0, column=0, sticky='nesw')
+        tk.Grid.rowconfigure(root,0,weight=1)
+        tk.Grid.columnconfigure(root,0,weight=1)
     def num2col(self, level, color):
         """ returns clean color of given level in form #xxxxxx """
         cval = hex(level).split("x")[1]
@@ -60,7 +64,11 @@ class colorBars(object):
                                outline = self.num2col(col,color))
 #            w.create_rectangle(col,self.bheight,col+self.bitwidth,2*self.bheight, fill = G, outline = G)
 #        self.root.mainloop()
-
+    def redraw(self, event):
+        self.width = event.width
+        self.height = event.height
+        self.drawBars()
+        tk.Event
 
 
 root = tk.Tk()
